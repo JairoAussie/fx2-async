1b547b87952af8803de62eb9fb9357a22eccffe7
jairoAussie <jairoaussie@gmail.com>
2021-04-28 12:17:46 +1000

    back

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let alertButton = document.querySelector("#showAlert")
+let quoteP = document.querySelector(".quote")
+let sourceP = document.querySelector(".source")
+let quoteButton = document.getElementById("showQuote")
+let sourceButton = document.getElementById("showSource")
 
-alertButton.addEventListener('click', ()=>{
-    setTimeout(()=>alert("ALERT!!"), 5000)
+quoteButton.addEventListener('click', (e)=>{
+    //e.preventDefault()
+    if (quoteP.style.display == 'none'){
+        quoteButton.innerText = "Hide quote"
+        quoteP.style.display = 'block'
+    } else{
+        quoteButton.innerText = "Show quote"
+        quoteP.style.display = 'none'
+    }
+})
+
+const toggleSource = (e)=>{
+    //e.preventDefault()
+    if (sourceP.style.display == 'none'){
+        sourceButton.innerText = "Hide source"
+        sourceP.style.display = 'block'
+    } else{
+        sourceButton.innerText = "Show source"
+        sourceP.style.display = 'none'
+    }
+}
+
+sourceButton.addEventListener('click', ()=>{
+    setTimeout((toggleSource), 3000)
 })
